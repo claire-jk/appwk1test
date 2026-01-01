930e0f1bbf66f7d00738fcb003aea6f5acb2cead
claire-jk <15.29.15.29e@gmail.com>
2026-01-01 22:16:57 +0800

    name/number

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello RN!</Text>
+      <Text style={styles.text}>Name:陳筠蒨</Text>
+      <Text style={styles.text}>Student Number:111319040</Text>
     </View>
   );
 }
@@ -12,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5757574d',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#b885ffff', // 改變文字顏色
+    fontSize: 25,   // 可以調整字型大小
   },
 });
